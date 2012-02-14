add7ce30716d5b460089b3a2feea89a875e1085d
Jean Bilheux <bilheuxjm@ornl.gov>
2012-02-13 21:44:25 -0500

    Added 3 widgets

--- scripts/imagesAction.js
+++ scripts/imagesAction.js
@@ -9,6 +9,15 @@ $(document).ready(function() {
 	$("#a_camera").bind("mouseover", cameraOver);
 	$("#a_camera").bind("mouseout", cameraOut);
 
+	$("#a_rates").bind("mouseover", ratesOver);
+	$("#a_rates").bind("mouseout", ratesOut);
+
+	$("#a_instructions").bind("mouseover", instructionsOver);
+	$("#a_instructions").bind("mouseout", instructionsOut);
+
+	$("#a_store").bind("mouseover", storeOver);
+	$("#a_store").bind("mouseout", storeOut);
+
 	function newsBoardMouseOver() {
 		$('#a_news_board img').attr('src',"../images/NewsBoardOver.png");
 	};
@@ -33,4 +42,28 @@ $(document).ready(function() {
 		$("#a_camera img").attr('src','../images/cameraOff.png');
 	}
 	
+	function ratesOver() {
+		$("#a_rates img").attr('src','../images/ratesOver.png');
+	}
+	
+	function ratesOut() {
+		$("#a_rates img").attr('src','../images/ratesOff.png');
+	}
+
+	function instructionsOver() {
+		$("#a_instructions img").attr('src','../images/instructionsOver.png');
+	}
+	
+	function instructionsOut() {
+		$("#a_instructions img").attr('src','../images/instructionsOff.png');
+	}
+
+	function storeOver() {
+		$("#a_store img").attr('src','../images/storeOver.png');
+	}
+	
+	function storeOut() {
+		$("#a_store img").attr('src','../images/storeOff.png');
+	}
+
 });
